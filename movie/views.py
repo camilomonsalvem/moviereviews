37bfe76e5db5ee20fe1a472e310e28dc76183095
camilomonsalvem <monsalve88mm@gmail.com>
2024-08-01 10:33:33 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')  # Con esta opcion cuando se hace el request se hace 'quemando' el html
     #return render(request, 'home.html') # Con esta opcion se accede al archivo HTML de la carpeta templates
-    return render(request, 'home.html', {'name':'Greg Lim'})
+    return render(request, 'home.html', {'name':'Camilo Monsalve'})
 
 def about(request):
     return HttpResponse('<h1>Welcome to About Page</h1>')
