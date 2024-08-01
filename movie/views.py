604f5eff5c5a247e9a693c408bd1a451439ce220
camilomonsalvem <monsalve88mm@gmail.com>
2024-08-01 11:28:05 -0500

    busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 # Create your views here.
 
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')  # Con esta opcion cuando se hace el request se hace 'quemando' el html
     #return render(request, 'home.html') # Con esta opcion se accede al archivo HTML de la carpeta templates
-    return render(request, 'home.html', {'name':'Camilo Monsalve'})
+    #return render(request, 'home.html', {'name':'Camilo Monsalve'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
 
 def about(request):
     #return HttpResponse('<h1>Welcome to About Page</h1>')
     return render(request, 'about.html')
+
