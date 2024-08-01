845abc0b6bfea0e11382209d16c73fc26508bd40
camilomonsalvem <monsalve88mm@gmail.com>
2024-08-01 10:27:03 -0500

    primer commit

---
+++ movie/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    #return HttpResponse('<h1>Welcome to Home Page</h1>')  # Con esta opcion cuando se hace el request se hace 'quemando' el html
+    #return render(request, 'home.html') # Con esta opcion se accede al archivo HTML de la carpeta templates
+    return render(request, 'home.html', {'name':'Greg Lim'})
+
+def about(request):
+    return HttpResponse('<h1>Welcome to About Page</h1>')
